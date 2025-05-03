1efb9998af1b5022a00f8af950ec3260f720bd11
Fernandoprado-gt <fernandovpfernandes@gmail.com>
2025-05-03 14:07:38 -0300

    Update Footer.tsxIG
    alteração link instagram

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ const Footer = () => {
                 </svg>
               </a>
               <a href="#" className="bg-tendencia-dark p-2 rounded-full border border-tendencia-cyan/30 text-tendencia-cyan hover:bg-tendencia-cyan/20 transition-colors">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
+                <svg xmlns="https://www.instagram.com/fernandoprado.gt/" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
                   <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
